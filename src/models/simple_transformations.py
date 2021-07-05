15ad2bd35ee642d51b586aeea21284cebc3ebc82
Srujana Merugu <srujana@gmail.com>
2021-07-06 02:02:48 +0530

    Update and rename static_profile_to_model_param.py to simple_transformations.py

---
+++ src/models/simple_transformations.py
@@ -0,0 +1,14 @@
+from dataset.dataset import TabularDataset
+from models.base_models import BaseTransformationModel
+import enum
+
+class SimpleTransformationsModel(BaseTransformationModel):
+
+    def __init__(self,_dataspec: TabularDataSpec, _model_config):
+        super(BaseTransformationModel, self).__init__()
+        self.dataspec = _dataspec
+        #TODO: shall we assume _model_config is a list of transformations 
+        # to be applied to individual input columns - some classes already exist
+        
+    def _predict(independent_vals):
+      #TODO: need to predict the transformation
