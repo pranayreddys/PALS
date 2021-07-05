10c13627025f77184162a6239e85830ffe354af9
Srujana Merugu <srujana@gmail.com>
2021-07-05 21:27:32 +0530

    Update data_manipulation_utils.py

---
+++ src/utils/data_manipulation_utils.py
@@ -0,0 +1,18 @@
+import collections.abc
+
+def flatten(d, parent_key='', sep='_'):
+    """Flatten a nested dictionary
+    Args:
+        d (collections.abc.MutableMapping): input dictionary
+        parent_key (str):
+        sep (str):
+    Returns:
+    """
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.abc.MutableMapping):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
