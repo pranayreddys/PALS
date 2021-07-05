6cb3b57ccf28b4d85f46e4e0bec5c0ddf6c7a117
Srujana Merugu <srujana@gmail.com>
2021-07-05 21:24:13 +0530

    Update math_ml_utils.py

---
+++ src/utils/math_ml_utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from entities.key_entities import LossFunctionSpec
+import keras.metrics as km
+
+
+def evaluate(ground_truth: pd.DataFrame, predictions: pd.DataFrame, loss_functions: List[LossFunctionSpec],
+	weights: pd.DataFrame = None):
+	#TODO: adding this here since we are interested in evaluating multiple loss functions and need some additional control
+	#sanity check on ground truth and predictions size and weights if available
+	#loop over loss functions 
+	#check if computation is column wise or cell wise; 
+  #compute accordingly and aggregate
+	return loss_values
