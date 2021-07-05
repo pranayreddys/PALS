21ac876a323f3492cd3734408be8798a02eb2ba3
Srujana Merugu <srujana@gmail.com>
2021-07-05 20:53:06 +0530

    Update and rename time_series_dataset.py to dataset.py

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -20,8 +20,8 @@ class TabularDataset(pd.DataFrame):
 
  	def detailed_report(self, report_path:str):
  		#TODO-SKIP FOR NOW - NOT NEEDED
-    #individual column - availability, distribution 
-    #pairwise correlations
+    		#individual column - availability, distribution 
+    		#pairwise correlations
  		return
 
 class TimeSeriesDataset(pd.DataFrame):
@@ -44,7 +44,7 @@ class TimeSeriesDataset(pd.DataFrame):
 
  	def detailed_report(self, report_path:str):
  		# TODO - LATER --statsmodels - we can use Sharut's code
-    # individual column - availability, distribution
+   		# individual column - availability, distribution
  		# plot original data, rolling mean and variance for day, week of each series x id (e.g., person's BP)
  		# plot auto-correlation
  		# run ADF-stationarity test, time series decomposition for each series x id
